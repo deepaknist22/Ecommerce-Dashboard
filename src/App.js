ff68440c262f6e01941ab7eda5fde05476a37a78
Deepak Kumar <deepaknist22@gmail.com>
2023-03-01 15:08:35 +0530

    Added File frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Nav from './Nav';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Nav />
+     <Routes>
+      <Route path='/' element={<h1>Product Page</h1>}></Route>
+      <Route path='/add' element={<h1> Add Product</h1>}></Route>
+      <Route path='/update' element={<h1>Update Product</h1>}></Route>
+      <Route path='/logout' element={<h1>Logout</h1>}></Route>
+      <Route path='/profile' element={<h1>Profile</h1>}></Route>
+     </Routes>
+
+      </BrowserRouter>
+
+       <Footer/>
+
     </div>
   );
 }
